4d2037c3ff86cf2598f6c9bdf4d083790fe24e8b
Anna Villarreal <154029176+AnnaVi11arrea1@users.noreply.github.com>
2025-01-15 22:02:52 +0000

    vendor event location now linked to event address through events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_01_12_030603) do
+ActiveRecord::Schema[7.1].define(version: 2025_01_15_215307) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -116,6 +116,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_01_12_030603) do
     t.float "profit"
     t.float "return"
     t.string "state"
+    t.string "address"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
