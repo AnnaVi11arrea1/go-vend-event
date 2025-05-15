7269ed27d2fde8b7db723bba14731da2042db5f9
Anna Villarreal <154029176+AnnaVi11arrea1@users.noreply.github.com>
2025-05-15 18:52:32 +0000

    putting all scrapper tasks into one scraper file - not sure if working codespaces maintenence?

--- lib/scrapers/ILFF_events_scraper.rb
+++ lib/scrapers/ILFF_events_scraper.rb
@@ -17,33 +17,25 @@ module Scrapers
         f.request :gzip # Enable gzip compression for requests
       end
 
-      # GET
       res = conn.get('/states/IL')
       doc = Nokogiri::HTML(res.body)
       articles = doc.css('.event')
 
       articles.each do |article|
-        title = article.at_css('h4')&.text&.strip.squish
+        title = article.at_css('h4')&.text&.strip
         date = article.at_css('.date')&.text&.strip
         location = article.at_css('.location')&.text&.strip
         link = article.at_css('a')
         link_href = link&.[]('href')
-
-                # Skip if event with the same name already exists
-        if Event.exists?(name: title, host_id: 1)
-          puts "Skipping duplicate event: #{title}"
-          next
-        end
         
-        event = Event.find_or_create_by(
-          name: title,
-          host_id: 1
-          ) do |e|
-          e.started_at = date,
-          e.tags = 'Illinois, Fairs and Festivals',
-          e.address = location,
-          e.application_link = link_href
-        end
+        event = Event.create(
+          started_at: date,
+          address: location,
+          application_link: link_href,
+          host_id: 1,
+          name: title
+        )
+        
         if event.persisted?
           puts "Event created: #{event.name} on #{event.started_at}"
         else
