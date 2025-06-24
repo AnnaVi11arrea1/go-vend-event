890830d67feb1b72fc8a27a311e67a39d91a52dd
Anna Villarreal <154029176+AnnaVi11arrea1@users.noreply.github.com>
2025-06-24 21:08:28 +0000

    run cron jobs without aws credentials - edited in carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,17 @@
-CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider:              'AWS',
-    aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    region:                ENV['AWS_REGION'],
-  }
-  config.fog_directory  = ENV['AWS_BUCKET']
-  config.fog_public     = true
+if ENV["AWS_ACCESS_KEY_ID"].present? && ENV["AWS_SECRET_ACCESS_KEY"].present?
+  CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider:              'AWS',
+      aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region:                ENV['AWS_REGION'],
+    }
+    config.fog_directory  = ENV['AWS_BUCKET']
+    config.fog_public     = true
+  end
+else
+  CarrierWave.configure do |config|
+    config.storage = :file
+  end
 end
--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,8 @@
 # Use this file to easily define all of your cron jobs.
 
 set :output, "log/cron_log.log"
-set :environment, 'production'
+set :environment, "production"
+env :RAILS_MASTER_KEY, ENV['RAILS_MASTER_KEY'] 
 
 every 2.weeks do
   rake "scraper:all"
